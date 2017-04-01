d84418b371c0a772bb60ab9a59189e9e6da2d0dc
Martin Broerse <broerse@martinic.com>
2017-04-01 09:08:14 +0200

    log id

--- app/routes/post.js
+++ app/routes/post.js
@@ -2,6 +2,10 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.findRecord('post', params.post_id);
+    return this.store.findRecord('post', params.post_id).then(function(data) {
+      console.log(data.get('id'));
+      console.log(data.get('title'));
+      return data;
+    });
   }
 });
